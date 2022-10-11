7eb059b47911f68f1091bdb1ddca30e2b9905915
askfnhzxkjcn <d.lee31@share.epsb.ca>
2022-10-11 14:48:12 -0600

    masodmasdo

--- spellcheck.py
+++ spellcheck.py
@@ -4,12 +4,51 @@
 # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
 
 import re  # Needed for splitting text with a regular expression
-
+import time
 
 def main():
-    # Load data files into lists
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
+
+    #start menu loop
+    loop = True
+
+    while loop:
+
+        #display
+        print("MAIN MENU")
+        print("1: Spell Check a Word (Linear Search)")
+        print("1: Spell Check a Word (Binary Search)")
+        print("1: Spell Check Alice In Wonderland (Linear Search")
+        print("4: Spell Check Alice In Wonderland (Binary Search)")
+        print("5: Exit")
+
+        #selection 1
+        selection = input("Unter menu selection (1-5): ")
+
+        #selection
+        if selection == "1":
+            print("poop")
+        
+        #selection 2
+        elif selection == "2":
+            print("poop")
+
+        #selection 3
+        elif selection == "3":
+            print("poop")
+
+        #selection 4
+        elif selection == "4":
+            print("poop")
+
+        #selection 5
+        else:
+            print("Goodbye")
+            loop = False
+    
+    # Load data files into lists
+    
 
     # Print first 50 values of each list to verify contents
     print(dictionary[0:50])
@@ -30,3 +69,7 @@ def loadWordsFromFile(fileName):
 
 # Call main() to begin program
 main()
+
+
+
+
